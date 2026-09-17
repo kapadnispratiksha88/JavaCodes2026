4335f00eef5a975a1a95cfb45ac2e4102ed37fa4
kapadnispratiksha88 <kapadnispratiksha88@gmail.com>
2026-09-17 15:40:17 +0530

    one is in the inheritance 2

---
+++ Dailycodes/inheritance2/p3.java
@@ -0,0 +1,33 @@
+class Demo {
+    int x=10;
+    static int y=20;
+    Demo(){
+        System.out.println("Demo contructpr");
+    }
+    void fun(){
+        System.out.println("in gun");
+    }
+    static void gun(){
+        System.out.println("inn fun");
+    }
+}
+class Demochild extends Demo{
+    Demochild(){
+    System.out.println("Demo child constructor");
+    }
+
+}
+class Client{
+    public static void main(String[] args) {
+        
+    
+    Demo obj =new Demo();
+    obj.fun();
+    obj.gun();
+    System.out.println(obj.x);
+    System.out.println(obj.y);
+    
+
+    }
+
+}
